7028ae8e3cca650d90c013cafce86e38faffc8ba
timo.wottka <timo.wottka@stud.hs-emden-leer.de>
2024-07-25 09:34:28 +0200

    Rewrite from Scratch started

--- Source/Core/Main.cpp
+++ Source/Core/Main.cpp
@@ -1,9 +1,7 @@
 #include "Utility/Log.h"
-#include "Engine.h"
 #include <memory>
 
-using namespace Corvus;
 int main() {
-    auto engine = std::make_unique<Engine>();
-    engine->run();
+    CORVUS_LOG(info, "Corvus Engine started");
+
 }
